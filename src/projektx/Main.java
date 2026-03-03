de55a309230400ef2d988ec89ef4eaefbd7694f7
fasmat16 <fasmat16@gasos-ro.cz>
2026-03-03 13:04:46 +0100

    faf

--- src/projektx/Main.java
+++ src/projektx/Main.java
@@ -2,7 +2,7 @@ package projektx;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Git");
+        System.out.println("Hello Git Lfkal");
 
     }
 }
